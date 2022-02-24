110b71753b7d9bdc186c24b6814a8148d0899460
강개굴 <kokk012@naver.com>
2022-02-24 17:39:09 +0900

    chap 3-6, finish

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pybo.apps.PyboConfig',
+    'common.apps.CommonConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -20,4 +20,7 @@ from pybo import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
+    path('common/', include('common.urls')),
+    # 디폴트 페이지를 설정하기 위한 코드 (여기서는 pybo의 views에 구현된 index 함수가 실행되어 게시판으로 이동됨)
+    path('', views.index, name='index'),
 ]
