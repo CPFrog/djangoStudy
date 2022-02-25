55687fc576924f3a7169016c511c01c38ede99af
강개굴 <kokk012@naver.com>
2022-02-25 16:31:39 +0900

    chap 3-9, finish

--- pybo/models.py
+++ pybo/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Q&A 게시판 생성
 
 # 질문 게시글에 필요한 내용
 class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)  # 익명질문이 가능하도록 질문 작성자에 null이 들어갈 수 있도록 함.
     subject = models.CharField(max_length=200)  # 질문 제목
     content = models.TextField()  # 질문 내용
     create_date = models.DateTimeField()  # 게시글 작성 시각
+    modify_date = models.DateTimeField(null=True, blank=True)  # 최종 수정 시각. 수정 안해서 null값을 가질 수 있으므로 이를 허용.
 
     def __str__(self):
         return self.subject
@@ -15,6 +18,8 @@ class Question(models.Model):
 
 # 답변 게시글에 필요한 내용
 class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)  # 답변을 달 질문 게시글 정보, 질문 게시글 삭제시 답변도 삭제.
     content = models.TextField()  # 답변 내용
     create_date = models.DateTimeField()  # 답변 게시글 작성 시각
+    modify_date = models.DateTimeField(null=True, blank=True)  # 최종 수정 시각. 수정 안해서 null값을 가질 수 있으므로 이를 허용.
--- pybo/views.py
+++ pybo/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Question
+from .models import Question, Answer
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 def index(request):
@@ -29,6 +31,7 @@ def detail(request, question_id):
     return render(request, 'pybo/question_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def question_create(request):
     """pybo 질문 등록"""
     # 질문 등록의 경우 get인 경우 질문 작성 양식만 가져오고, post인 경우 데이터가 저장되어야 하므로 각각에 대한 처리.
@@ -36,6 +39,7 @@ def question_create(request):
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)  # 임시 저장 아님
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo:index')
@@ -45,6 +49,39 @@ def question_create(request):
     return render(request, 'pybo/question_form.html', context)
 
 
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    """ 게시글 수정 """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '게시글 관리 권한이 없습니다.')
+        return redirect('pybo:detail', question_id=question.id)
+
+    if request.method == 'POST':
+        form = QuestionForm(request.POST, instance=question)  # 기존에 저장된 제목, 내용이 반영된 작성 양식 불러오기 위해 instance 설정
+        if form.is_valid():
+            question = form.save(commit=False)  # 임시 저장 아님
+            question.author = request.user
+            question.modify_date = timezone.now()
+            question.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:  # else로 걸러지는 대부분의 요청방식은 get일 것이고, 이 경우 입력값 없이 객체 생성.
+        form = QuestionForm(instance=question)
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
+
+
+def question_delete(request, question_id):
+    """게시글 삭제"""
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '게시글 관리 권한이 없습니다.')
+        return redirect('pybo:detail', question_id=question.id)
+    question.delete()
+    return redirect('pybo:index')
+
+
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     """
     pybo 답변 등록
@@ -55,6 +92,7 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user  # 답변 작성자 정보 등록
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
@@ -63,3 +101,37 @@ def answer_create(request, question_id):
         form = AnswerForm()
         context = {'question': question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
+
+
+@login_required(login_url='common:login')
+def answer_modify(request, answer_id):
+    """ 답변 수정 """
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '게시글 관리 권한이 없습니다.')  # request 빼먹어서 오류 뜸
+        return redirect('pybo:detail', question_id=answer.question.id)
+
+    if request.method == 'POST':
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('pybo:detail', question_id=answer.question.id)
+    # else문 들여쓰기 착각으로 인해 20분동안 헤맴
+    else:
+        form = AnswerForm(instance=answer)
+    context = {'answer': answer, 'form': form}
+    return render(request, 'pybo/answer_form.html', context)
+
+
+@login_required(login_url='common:login')
+def answer_delete(request, answer_id):
+    """ 답변 삭제 """
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '게시글 관리 권한이 없습니다.')
+    else:
+        answer.delete()
+    return redirect('pybo:detail', question_id=answer.question.id)
