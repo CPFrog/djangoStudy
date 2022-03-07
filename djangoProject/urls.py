1a0024af07d3d10efa899f8d83bb6242c4377b41
강개굴 <kokk012@naver.com>
2022-03-07 14:58:53 +0900

    chap 3-11 split views.py, modified import routine in each file

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from pybo import views
+from pybo.views import base_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
     path('common/', include('common.urls')),
     # 디폴트 페이지를 설정하기 위한 코드 (여기서는 pybo의 views에 구현된 index 함수가 실행되어 게시판으로 이동됨)
-    path('', views.index, name='index'),
+    # 3-11장에서 views 파일을 각각의 역할에 따라 별도의 파일로 분리하였으므로 여기서도 url에 대한 수정 필요.
+    path('', base_views.index, name='index'),
 ]
--- pybo/urls.py
+++ pybo/urls.py
@@ -1,22 +1,32 @@
 from django.urls import path
-
-from . import views
+from .views import base_views, question_views, answer_views, comment_views
+# 3-11장에서 views.py를 각 게시글 종류에 따라 역할을 담당하는 여러개의 *_views.py 파일로 나눔에 따라 참조 위치도 바뀜.
 
 app_name = 'pybo'
 
 urlpatterns = [
-    path('', views.index, name='index'),  # URL 별칭-->index
-    path('<int:question_id>/', views.detail, name='detail'),  # URL 별칭-->detail
-    path('answer/create/<int:question_id>', views.answer_create, name='answer_create'),
-    path('question/create/', views.question_create, name='question_create'),
-    path('question/modify/<int:question_id>/', views.question_modify, name='question_modify'),
-    path('question/delete/<int:question_id>', views.question_delete, name='question_delete'),
-    path('answer/modify/<int:answer_id>', views.answer_modify, name='answer_modify'),
-    path('answer/delete/<int:answer_id>', views.answer_delete, name='answer_delete'),
-    path('comment/create/question/<int:question_id>/', views.comment_create_question, name='comment_create_question'),
-    path('comment/modify/question/<int:comment_id>/', views.comment_modify_question, name='comment_modify_question'),
-    path('comment/delete/question/<int:comment_id>/', views.comment_delete_question, name='comment_delete_question'),
-    path('comment/create/answer/<int:answer_id>/', views.comment_create_answer, name='comment_create_answer'),
-    path('comment/modify/answer/<int:comment_id>/', views.comment_modify_answer, name='comment_modify_answer'),
-    path('comment/delete/answer/<int:comment_id>/', views.comment_delete_answer, name='comment_delete_answer'),
+    # views -> base_views : 기초
+    path('', base_views.index, name='index'),  # URL 별칭-->index
+    path('<int:question_id>/', base_views.detail, name='detail'),  # URL 별칭-->detail
+
+    # views -> question_views : 질문 게시글 관련
+    path('question/create/', question_views.question_create, name='question_create'),
+    path('question/modify/<int:question_id>/', question_views.question_modify, name='question_modify'),
+    path('question/delete/<int:question_id>', question_views.question_delete, name='question_delete'),
+
+    # views -> answer_views : 답변 게시글 관련
+    path('answer/create/<int:question_id>', answer_views.answer_create, name='answer_create'),
+    path('answer/modify/<int:answer_id>', answer_views.answer_modify, name='answer_modify'),
+    path('answer/delete/<int:answer_id>', answer_views.answer_delete, name='answer_delete'),
+
+    # views -> comment_views : 댓글 관련
+    path('comment/create/question/<int:question_id>/', comment_views.comment_create_question,
+         name='comment_create_question'),
+    path('comment/modify/question/<int:comment_id>/', comment_views.comment_modify_question,
+         name='comment_modify_question'),
+    path('comment/delete/question/<int:comment_id>/', comment_views.comment_delete_question,
+         name='comment_delete_question'),
+    path('comment/create/answer/<int:answer_id>/', comment_views.comment_create_answer, name='comment_create_answer'),
+    path('comment/modify/answer/<int:comment_id>/', comment_views.comment_modify_answer, name='comment_modify_answer'),
+    path('comment/delete/answer/<int:comment_id>/', comment_views.comment_delete_answer, name='comment_delete_answer'),
 ]
