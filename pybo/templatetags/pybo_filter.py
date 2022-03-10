e1b09fdfbc681faaabaf4c51215e82ad4e82afc9
강개굴 <kokk012@naver.com>
2022-03-10 18:11:17 +0900

    chap 3-14 - 마크다운 문법 기능 추가 (질문, 답변 게시글 에디터)

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -1,4 +1,6 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -6,3 +8,15 @@ register = template.Library()
 @register.filter
 def sub(value, arg):
     return value - arg
+
+
+# 마크 다운 적용하는 코드
+@register.filter()
+def mark(value):
+    extensions = ['nl2br', 'fenced_code']
+    return mark_safe(markdown.markdown(value, extensions=extensions))
+
+# 마크다운 적용 후 작성한 결과
+# 1. 1. 1. 식으로 해도 넘버링 적용 안됨
+# > 적용 안됨.
+
